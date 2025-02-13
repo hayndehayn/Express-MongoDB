d5b0b6232dbb57fe5144ffe0ba2538a038fe622b
artem <vualkalox1@gmail.com>
2025-02-13 10:20:37 +0100

    Refacted ver.

--- pugproj/server.js
+++ pugproj/server.js
@@ -3,12 +3,13 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-// Налаштування шляху до папки views
+// Path to views
 app.set("views", path.join(__dirname, "views"));
 
-// Налаштування шаблонізаторів
-app.set("view engine", "pug");
-app.set("view engine", "ejs");
+// Engine set-up
+app.set("view engine", "pug"); // Default engine
+app.engine('pug', require('pug').__express);
+app.engine('ejs', require('ejs').__express);
 
 // CSS
 app.use(express.static("public"));
@@ -58,7 +59,7 @@ app.get("/articles/:articleId", (req, res) => {
     res.render("articles/details", { article });
 });
 
-// Запуск сервера
+// Server start
 app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 });
